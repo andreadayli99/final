8a70433882cce56c3201ba3a9cc95bf4d9706cfd
Dayli <daylandrea01@gmail.com>
2025-03-17 21:42:59 -0500

    segunda version

--- data_transform.py
+++ data_transform.py
@@ -9,7 +9,8 @@ try:
     
     # Ordenar los datos por nombre
     data_ordenada = data.sort_values(by='Nombre1')
-    
+    # Regla 2: Normalizar los nombres (conmayúsculas y sin caracteres especiales)
+ # Regla 3: Corregir fechas inválidas de nacimiento.    
     # Exportar a Excel
     archivo_excel = 'clientes_ordenados.xlsx'
     data_ordenada.to_excel(archivo_excel, index=False)
